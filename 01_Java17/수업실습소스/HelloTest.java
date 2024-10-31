87942e339e9113c45b2730be7c26547cde65c691
inky4832 <inky4832@daum.net>
2024-10-31 17:16:04 +0900

    Add files via upload

---
+++ 01_Java17/수업실습소스/HelloTest.java
@@ -0,0 +1,12 @@
+
+public class HelloTest {
+
+	public static void main(String[] x) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		System.out.println('남');
+		System.out.println(172+10);
+		System.out.println(182.65);
+		System.out.println(true);
+	}
+}
