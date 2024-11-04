ef735e2de01ba068aacb31f95b8ead2967dd72d4
inky4832 <inky4832@daum.net>
2024-11-04 15:46:18 +0900

    Add files via upload

---
+++ 01_Java17/수업실습소스/exam07_제어문01_조건문/Statement03_다중if문.java
@@ -0,0 +1,23 @@
+package exam07_제어문01_조건문;
+
+import java.util.Scanner;
+
+public class Statement03_다중if문 {
+
+	public static void main(String[] args) {
+		
+		// 다중 if문: 여러번 조건식을 이용해서 비교할 때.
+		
+		// 문제1: 키보드로 정수값을 입력받고 입력받은 등급을 구하시오. 단 입력정수값은 1~100 범위만 입력가능함.
+		/*
+		 *   90~100: A등급
+		 *   80~89: B등급
+		 *   70~79: C등급
+		 *   나머지(1~69): D등급
+		 * 
+		 */
+	
+		
+		
+	}
+}
