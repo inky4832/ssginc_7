9067a5fe7bfdc398b94918c2abbc560446b7cbd7
inky4832 <inky4832@daum.net>
2024-11-05 10:50:04 +0900

    Add files via upload

---
+++ 01_Java17/수업실습소스/exam07_제어문02_반복문/Statement03_do_while문.java
@@ -0,0 +1,31 @@
+package exam07_제어문02_반복문;
+
+public class Statement03_do_while문 {
+
+	public static void main(String[] args) {
+	
+		// for 문
+		for( int n=1 ; n < 6  ; n++) {
+			System.out.println("hello");
+		}
+		
+		
+		// while 문
+		int n=1 ;
+		while(n < 6) {
+			System.out.println("world");
+			 n++;
+		}
+		
+		// do~while문
+		int n2=1 ;
+		do {
+			System.out.println("happy");
+			 n2++;
+		}while(n2 < 6);
+		
+		
+		
+		System.out.println("END");
+	}
+}
