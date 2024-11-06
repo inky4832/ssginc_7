89a4a164bee5b769479f996a660d5f96aa6d03b4
inky4832 <inky4832@daum.net>
2024-11-06 15:48:36 +0900

    Add files via upload

--- 01_Java17/수업실습소스/exam10_생성자/Cat.java
+++ 01_Java17/수업실습소스/exam10_생성자/Cat.java
@@ -8,18 +8,18 @@ public class Cat {
 	
 	//생성자
 	public Cat() {
-		System.out.println("Cat 생성자");
+		System.out.println("Cat() 생성자");
 	}
-	public Cat(int n) {
-		System.out.println("Cat 생성자");
+	public Cat(int x) {
+		System.out.println("Cat(int n) 생성자" + x);
 	}
 	public Cat(String n) {
-		System.out.println("Cat 생성자");
+		System.out.println("Cat(String n) 생성자 " + n);
 	}
 	public Cat(String n, int x) {
-		System.out.println("Cat 생성자");
+		System.out.println("Cat(String n, int x) 생성자 " + n +" " + x);
 	}
 	public Cat(int x, String n) {
-		System.out.println("Cat 생성자");
+		System.out.println("Cat(int x, String n) 생성자 "+ x +" " + n);
 	}
 }
--- 01_Java17/수업실습소스/exam10_생성자/TestCat.java
+++ 01_Java17/수업실습소스/exam10_생성자/TestCat.java
@@ -6,6 +6,10 @@ public class TestCat {
 		
 		// 객체생성(인스턴스화)
 		Cat c = new Cat();
+		Cat c2 = new Cat(2);
+		Cat c3 = new Cat("나비");
+		Cat c4 = new Cat("나비",2);
+		Cat c5 = new Cat(2, "나비");
 
 	}
 
