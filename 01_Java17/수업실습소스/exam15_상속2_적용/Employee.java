597e26446a03a3cdfb91e9a4a001c539342ec796
inky4832 <inky4832@daum.net>
2024-11-12 11:47:16 +0900

    Add files via upload

---
+++ 01_Java17/수업실습소스/exam15_상속2_적용/Employee.java
@@ -0,0 +1,29 @@
+package exam15_상속2_적용;
+
+public class Employee { // extends Object 동일
+
+	String name;
+	int salary;
+	
+	public Employee() {}
+	public Employee(String name, int salary) {
+		this.name = name;
+		this.salary = salary;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getSalary() {
+		return salary;
+	}
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+}
