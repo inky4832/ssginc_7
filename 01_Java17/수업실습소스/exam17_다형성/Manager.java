0e26ea0216dffcba612f543d7675472f4a492fd0
inky4832 <inky4832@daum.net>
2024-11-13 10:53:58 +0900

    Add files via upload

---
+++ 01_Java17/수업실습소스/exam17_다형성/Manager.java
@@ -0,0 +1,34 @@
+package exam17_다형성;
+
+public class Manager extends Employee{
+	
+	String depart; // Manager에 추가된 속성
+	String email;
+	
+	public Manager() {
+		super();
+	}
+	public Manager(String name, int salary) {
+		super(name, salary);
+	}
+	public Manager(String name, int salary, String depart) {
+		super(name, salary);
+		this.depart = depart;
+	}
+	public String getDepart() {
+		return depart;
+	}
+	public void setDepart(String depart) {
+		this.depart = depart;
+	}
+	@Override
+	public String toString() {
+		return "Manager [depart=" + depart + ", name=" + name + ", salary=" + salary + "]";
+	}
+
+	//자식에만 있는 메서드
+	public void xxx() {
+		
+	}
+	
+}
