adcf0ce277293cd41e36afbc5ac123506b642c20
inky4832 <inky4832@daum.net>
2024-11-20 10:36:47 +0900

    Add files via upload

--- 01_Java17/수업실습소스/exam27_Arrays_Collections유틸리티/CollectionsTest.java
+++ 01_Java17/수업실습소스/exam27_Arrays_Collections유틸리티/CollectionsTest.java
@@ -2,7 +2,27 @@ package exam27_Arrays_Collections유틸리티;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+
+class Cat{
+	
+	String name;
+	int age;
+	public Cat() {
+		super();
+	}
+	public Cat(String name, int age) {
+		super();
+		this.name = name;
+		this.age = age;
+	}
+	//getter/setter
+	@Override
+	public String toString() {
+		return "Cat [name=" + name + ", age=" + age + "]";
+	}
+}//end Cat
 
 public class CollectionsTest {
 
@@ -22,6 +42,54 @@ public class CollectionsTest {
 		 System.out.println("2. copy: x" + x); // [10, 20, 30, 40, 67]
 		 System.out.println("2. copy: x2" + x2); // [10, 20, 30, 40]
 		 
-	}
+		 //2. sort 기능
+		 List<Cat> catList =  Arrays.asList( new Cat("c5", 2),
+											 new Cat("c4", 5),
+											 new Cat("c2", 7),
+											 new Cat("c1", 10),
+											 new Cat("c3", 1)
+				                           );
+		 System.out.println(catList);
+		 // Collections.sort( 리스트, Comparator타입)
+		 /*
+		      Comparator 인터페이스의 compare(값1, 값2) 추상메서드를 재정의해서
+		      Cat의 어떤 값으로 정렬할지를 알려준다.
+		      
+		      Comparator 인터페이스 사용방법
+		      1) 이름있는 클래스 이용.
+		         예> public class XXX implments Comparator{compare재정의}
+		         
+		      2) 이름없는 클래스(익명클래스) 이용
+		       예>  Comparator c = new Comparator(){
+		                 compare재정의
+		           }
+		      3) 람다표현식 이용
+		         Comparator c = (값1, 값2)->{};
+		       
+		  */
+		  // 이름없는 클래스(익명클래스) 이용
+		  Comparator<Cat> comp = new Comparator<Cat>() {
+			
+			@Override
+			public int compare(Cat c1, Cat c2) {
+//				return c1.age - c2.age; // 오름차순 
+				return -(c1.age - c2.age); // 내림차순 
+			}
+		 };
+		 // 람다 표현식
+		 Comparator<Cat> comp2 = (Cat c1, Cat c2)->{
+//				return c1.age - c2.age; // 오름차순 
+				return -(c1.age - c2.age); // 내림차순 
+			};
+		 Comparator<Cat> comp3 = (c1, c2)-> -(c1.age - c2.age); // 내림차순 
+//		  Collections.sort(catList, comp3);
+		 
+		  Collections.sort(catList, (c1, c2)-> -(c1.age - c2.age));
+		  
+		  System.out.println(catList);
+		 
+		 
+		 
+	}//end main
 
 }
