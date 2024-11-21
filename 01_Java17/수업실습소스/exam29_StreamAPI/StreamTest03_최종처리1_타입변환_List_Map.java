9f8e21ab36a12b1d27aff9989a0f3f875d52fb1f
inky4832 <inky4832@daum.net>
2024-11-21 15:26:13 +0900

    Add files via upload

--- 01_Java17/수업실습소스/exam29_StreamAPI/StreamTest03_최종처리1_타입변환_List_Map.java
+++ 01_Java17/수업실습소스/exam29_StreamAPI/StreamTest03_최종처리1_타입변환_List_Map.java
@@ -3,6 +3,7 @@ package exam29_StreamAPI;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.function.ToIntFunction;
@@ -69,6 +70,34 @@ public class StreamTest03_최종처리1_타입변환_List_Map {
 		//3. 이름만 리스트로 반환
 		List<String> nameList = list.stream().map( Person4::getName).collect(Collectors.toList());
 		System.out.println(nameList);  // [홍길동, 이순신, 유관순, 정조, 강감찬, 세종대왕]
+		
+		/////////////////////////////////////////////////////////////////////////////////
+		// Map 반환: Map xxx =  stream().collect( Collectors.toMap(Function key, Function value) );
+		// key는 이름, value는 나이
+		Function<Person4, String> keyFun = new Function<Person4, String>() {
+			@Override
+			public String apply(Person4 t) {
+				return t.getName();
+			}
+		};
+		Function<Person4, Integer> valueFun = new Function<Person4, Integer>() {
+			@Override
+			public Integer apply(Person4 t) {
+				return t.getAge();
+			}
+		};
+		Function<Person4, String> keyFun2 = t->t.getName();  // 람다 표현식
+		Function<Person4, String> keyFun3 = Person4::getName; // 메서드 참조
+
+		Function<Person4, Integer> valueFun2 = t->t.getAge();  // 람다 표현식
+		Function<Person4, Integer> valueFun3 = Person4::getAge; // 메서드 참조
+		
+//		Map<String, Integer> map = list.stream().collect(Collectors.toMap(keyFun3, valueFun3));
+		Map<String, Integer> map = list.stream()
+				                       .collect(Collectors.toMap(Person4::getName, Person4::getAge));
+		System.out.println(map);  // {정조=65, 홍길동=10, 강감찬=34, 이순신=30, 유관순=14, 세종대왕=23}
+		
+		
 		
 	}
 }
