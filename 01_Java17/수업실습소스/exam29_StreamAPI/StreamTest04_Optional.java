a0a6570ae556d4de343256fdede35988a88ef218
inky4832 <inky4832@daum.net>
2024-11-21 16:08:26 +0900

    Add files via upload

---
+++ 01_Java17/수업실습소스/exam29_StreamAPI/StreamTest04_Optional.java
@@ -0,0 +1,12 @@
+package exam29_StreamAPI;
+
+public class StreamTest04_Optional {
+
+	public static void main(String[] args) {
+		// OptionalXXX : 배열이나 리스트에 값이 없는 경우에
+		//            그룹핑 메서드를 사용하면 NullPointerException이 발생될 수 있다.
+		//            이때 NullPointerException 발생을 방지할 수 있다
+
+	}
+
+}
