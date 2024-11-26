eac2bf8256c03b715025c1ee549940760a94b7a9
inky4832 <inky4832@daum.net>
2024-11-26 09:32:34 +0900

    Add files via upload

--- 02_SQL/scott.sql
+++ 02_SQL/scott.sql
@@ -91,3 +91,289 @@ SELECT empno, ename, job, deptno
  
  select sal, sal % 3 , mod(sal , 3)
  from emp;
+ 
+-- 8. like 연산자
+use testdb;
+ SELECT empno, ename, sal
+ FROM emp
+ WHERE ename LIKE 'A%';
+ 
+ SELECT empno, ename, sal
+ FROM emp
+ WHERE ename LIKE '%T%';
+ 
+SELECT empno, ename, sal
+ FROM emp
+ WHERE ename LIKE '_L%';
+ 
+ SELECT empno, ename, sal
+ FROM emp
+ WHERE ename LIKE '____N';  -- 이름이 5글자이고 N으로 끝남
+ 
+SELECT empno, ename, sal
+ FROM emp
+ WHERE ename LIKE '%E_%' ESCAPE 'E';
+ 
+ -- 9. 논리 연산자
+ SELECT empno, ename, job, deptno, hiredate, sal
+ FROM emp
+ WHERE job = 'salesman'AND sal >= 1500;
+ 
+  SELECT empno, ename, job, deptno, hiredate, sal
+ FROM emp
+ WHERE job = 'salesman' OR sal >= 1500;
+ 
+  SELECT empno, ename, sal, comm, job
+ FROM emp
+ WHERE comm IS NOT NULL;
+ 
+  SELECT empno, ename, sal
+ FROM emp
+ WHERE ename NOT LIKE 'A%';
+ 
+  SELECT empno, ename, job, deptno, sal
+ FROM emp
+ WHERE sal NOT BETWEEN 800 AND 2000; 
+ 
+ -- 10. 정렬
+ SELECT empno, ename, hiredate
+ FROM emp
+ ORDER BY hiredate;  -- 컬럼명 지정
+ 
+ SELECT empno, ename, hiredate
+ FROM emp
+ ORDER BY hiredate  desc ;
+ 
+  SELECT empno, ename, sal * 12 AS Annual
+ FROM emp
+ ORDER BY Annual;  -- 별칭 지정 
+ 
+SELECT empno, ename, sal * 12 AS Annual
+ FROM emp
+ ORDER BY 1;   -- select절에 지정된 컬럼 순서값 지정
+ 
+ -- 다중 정렬
+ SELECT empno, ename, sal * 12 AS Annual
+ FROM emp
+ ORDER BY 3, 1 DESC;
+ 
+ 
+ -- 11. 단일행 함수 - 문자데이터
+ SELECT empno, lower(ename), LOWER('HeLLO')
+ FROM emp;
+ 
+SELECT empno, UPPER(ename), UPPER('HeLLO')
+ FROM emp;
+ 
+SELECT empno, CONCAT(ename, ' 과 ', sal) as "ename sal"
+ FROM emp;
+ 
+ SELECT empno, CONCAT_WS(',', ename, sal)
+ FROM emp;
+ 
+ SELECT empno, LPAD(ename,10, '*'), LPAD(sal, 10, '*')
+ FROM emp;
+
+ SELECT empno, RPAD(ename,10, '*'), RPAD(sal, 10, '*')
+ FROM emp;
+ 
+  SELECT empno, ename, SUBSTR(ename,1,2), SUBSTR(ename,3),
+ SUBSTR(ename,-1)
+ FROM emp;
+ 
+ SELECT empno, ename, LENGTH(ename)
+ FROM emp;
+
+ SELECT empno, ename, sal, REPLACE(sal, '0','o')
+ FROM emp;
+
+SELECT INSTR('foobarbar', 'bar'), INSTR('foobarbar', 'xbar')
+from dual;
+
+SELECT LTRIM('     bar     '), LENGTH(LTRIM('     bar     '));
+
+SELECT RTRIM('     bar     '), LENGTH(RTRIM('     bar     '));
+
+SELECT TRIM('     bar     '),
+ TRIM(BOTH FROM '     bar     '),
+ TRIM(LEADING FROM '     bar     '),
+ TRIM(TRAILING FROM '     bar     ');
+ 
+ SELECT TRIM('xxxbarxxx'),
+ TRIM(BOTH 'x' FROM 'xxxbarxxx'),
+ TRIM(LEADING 'x' FROM 'xxxbarxxx'),
+ TRIM(TRAILING 'x' FROM 'xxxbarxxx');
+ 
+SELECT ename, REVERSE(ename)
+ FROM emp;
+
+SELECT FORMAT(9876543.2195, 2);  -- 반올림해서 소수점 2자리까지 표현
+-- insert(str,pos,len,newstr)
+select insert('abcdefg',3, 4, '####');
+
+SELECT LEFT('foobarbar', 4), RIGHT('foobarbar', 4);
+
+SELECT REPEAT('MySQL', 3);
+
+SELECT empno, CONCAT(ename, '     ', sal) as "ename sal"
+ FROM emp;
+SELECT empno, CONCAT(ename, space(5), sal) as "ename sal"
+ FROM emp;
+ 
+-- 12. 단일행함수 - 수치 함수
+SELECT ROUND(45.678), ROUND(45.678, 2) , ROUND(45.678,-1) FROM DUAL;
+
+ SELECT TRUNCATE(45.678, 0), TRUNCATE(45.678, 2) ,
+ TRUNCATE(45.678,-1)
+ FROM DUAL;
+
+ SELECT CEIL(45.178), CEIL(-45.178);
+  SELECT FLOOR(45.178), FLOOR(-45.178);
+  
+  SELECT MOD(10, 3), 10%3 , 10 MOD 3 FROM DUAL;
+SELECT SIGN(-1.200), SIGN(34.3), SIGN(0) FROM DUAL;  
+
+SELECT abs(-1.200), abs(34.3), abs(0) FROM DUAL;  
+
+-- 12. 단일행 함수 - 날짜 함수
+ -- 현재 날짜 구하기 :  'yyyy-MM-dd'
+ SELECT CURDATE(), CURRENT_DATE(), CURRENT_DATE FROM DUAL;
+ 
+ -- 현재 시간 구하기 : 'hh:mm:ss'
+  SELECT CURTIME(), CURRENT_TIME(), CURRENT_TIME FROM DUAL;
+  
+-- 현재날짜 및 시간 구하기 : 'YYYY-MM-DD hh:mm:ss' 
+ SELECT SYSDATE(), NOW();
+ SELECT SYSDATE(), NOW() FROM DUAL;
+ 
+ -- NOW()는 select 문이 실행될때 시간을 반환
+SELECT NOW(), sleep(2), NOW() FROM DUAL;
+ -- SYSDATE()는 SYSDATE()함수가 호출되어 실행될 때의 시간을 반환.
+SELECT SYSDATE(), sleep(2), SYSDATE() FROM DUAL;
+ 
+SELECT ADDDATE('2008-01-02', 30), ADDDATE('2008-01-02',-30);
+SELECT ADDDATE( NOW(), 30), ADDDATE(NOW(), -30);
+
+SELECT DATE_ADD('2008-01-02', INTERVAL 1 DAY) as A1,
+ DATE_ADD('2008-01-02', INTERVAL 1 MONTH) as A2,
+ DATE_ADD('2008-01-02', INTERVAL 1 YEAR) as A3,
+ NOW() as A4,
+ DATE_ADD(NOW(), INTERVAL -10 MINUTE) as A5,
+ DATE_ADD(NOW(), INTERVAL 2 HOUR) as A6;
+ 
+  SELECT DATE_SUB('2008-01-02', INTERVAL 1 DAY) as B1,
+ DATE_SUB('2008-01-02', INTERVAL 1 MONTH) as B2,
+ DATE_SUB('2008-01-02', INTERVAL 1 YEAR) as B3,
+ NOW() as B4,
+ DATE_SUB(NOW(), INTERVAL 10 MINUTE) as B5,
+ DATE_SUB(NOW(), INTERVAL 2 HOUR) as B6;
+ 
+ SELECT DATEDIFF('2023-01-04', '2022-01-04');
+ 
+ SELECT TIMESTAMPDIFF(HOUR, '2020-3-1', '2020-3-3') AS C1,
+ TIMESTAMPDIFF(DAY, '2020-3-1', '2020-3-3') AS C2,
+ TIMESTAMPDIFF(MONTH, '2020-2-1', '2020-6-3') AS C3,
+ TIMESTAMPDIFF(YEAR, '2020-3-1', '2022-3-3') AS C4;
+ 
+ SELECT LAST_DAY('2003-02-05'),LAST_DAY(NOW());
+ 
+SELECT NOW(),
+ EXTRACT(SECOND FROM NOW()),
+ EXTRACT(MINUTE FROM NOW()),
+ EXTRACT(HOUR FROM NOW()),
+ EXTRACT(DAY FROM NOW()),
+ EXTRACT(MONTH FROM NOW()),
+ EXTRACT(YEAR FROM NOW()),
+ EXTRACT(YEAR_MONTH FROM NOW());
+ 
+ SELECT NOW(),
+ DATE_FORMAT(NOW(),'%Y%m%d'),
+ DATE_FORMAT(NOW(),'%Y/%m/%d'),
+ DATE_FORMAT(NOW(),'%Y년%m월 %d일'),
+ DATE_FORMAT(NOW(),'%H:%i:%S'),
+ DATE_FORMAT(NOW(),'%Y');
+ 
+ SELECT STR_TO_DATE('2020-03-04','%Y-%m-%d'),
+ STR_TO_DATE('01,5,2013','%d,%m,%Y'),
+ STR_TO_DATE('2020년03월05일','%Y년%m월%d일'),
+ STR_TO_DATE('2020-03-04 12:23:32','%Y-%m-%d %H:%i:%S');
+ 
+ -- 13. 단일행 함수 - 조건 함수
+ -- case 문- 동등비교
+ select empno, ename, sal, job , CASE job WHEN 'ANALYST' THEN sal * 1.1 
+                                          WHEN 'CLERK' THEN sal * 1.2
+                                          WHEN 'MANAGER' THEN sal * 1.3
+                                          WHEN 'PRESIDENT' THEN sal * 1.4
+                                          WHEN 'SALESMAN' THEN sal * 1.5
+										  ELSE sal
+								 END  as 실수령 
+ from emp;
+ 
+  SELECT empno, ename, sal,
+						 CASE WHEN sal > 3500 THEN '이사급'
+							  WHEN sal > 2500 THEN '과장급'
+						      ELSE '사원급' 
+                         END AS직급연봉
+FROM emp;
+
+ SELECT sal, IF(sal > 2500 , '과장이상급', '사원급')
+ FROM emp;
+ 
+ -- 14. 단일행 함수 - 변환함수
+ select cast(10 as char), cast('10' as SIGNED INTEGER), cast(10 as float),
+		cast('2024-11-25' as Date);
+        
+        
+-- 15. 그룹함수
+SELECT AVG(sal), MAX(sal), MIN(sal), SUM(sal)
+ FROM emp
+ WHERE job = 'SALESMAN';
+ 
+  SELECT COUNT(*), COUNT(comm),
+ AVG(comm), AVG(IFNULL(comm,0)),
+ COUNT(deptno), COUNT(DISTINCT deptno)
+ FROM emp;
+ 
+  -- group by 컬럼명
+  SELECT deptno as no, COUNT(*), 
+          ROUND(AVG(sal)), 
+          MIN(sal), 
+          MAX(sal), 
+          SUM(sal)
+ FROM emp
+ GROUP BY deptno;
+ -- group by 별칭
+   SELECT deptno as no, COUNT(*), 
+          ROUND(AVG(sal)), 
+          MIN(sal), 
+          MAX(sal), 
+          SUM(sal)
+ FROM emp
+ GROUP BY no;
+ 
+  -- group by 컬럼순서
+   SELECT deptno as no, COUNT(*), 
+          ROUND(AVG(sal)), 
+          MIN(sal), 
+          MAX(sal), 
+          SUM(sal)
+ FROM emp
+ GROUP BY 1;
+ 
+  SELECT deptno, COUNT(*), ROUND(AVG(sal)), MIN(sal), MAX(sal), SUM(sal)
+ FROM emp
+ GROUP BY deptno
+ ORDER BY SUM(sal) DESC;
+ 
+  SELECT deptno, SUM(sal)
+ FROM emp
+ GROUP BY deptno
+ HAVING SUM(sal) > 7000;
+ 
+ SELECT deptno, SUM(sal)
+ FROM emp
+ WHERE sal > 1500
+ GROUP BY deptno
+ HAVING SUM(sal) > 7000;
+ 
+ 
