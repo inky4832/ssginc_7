60c8de97fbc5c3a906c2aa28a6c52a7295d9e2a5
inky4832 <inky4832@daum.net>
2024-12-03 13:46:26 +0900

    Add files via upload

--- 04_MyBatis/수업실습소스2_동적SQL/MyBatisMainTest.java
+++ 04_MyBatis/수업실습소스2_동적SQL/MyBatisMainTest.java
@@ -24,23 +24,52 @@ public class MyBatisMainTest {
 		
 		
 		//2. 멀티 delete
-		List<Integer> delList = Arrays.asList(1001, 1002);
-		int n2 = session.delete("com.config.EmpMapper.deleteMulti", delList);
-		session.commit();
-		System.out.println(n2 +" 개가 삭제됨");
-		System.out.println("####################################");
+//		System.out.println("2. 멀티 delete ####################################");
+//		List<Integer> delList = Arrays.asList(1001, 1002);
+//		int n2 = session.delete("com.config.EmpMapper.deleteMulti", delList);
+//		session.commit();
+//		System.out.println(n2 +" 개가 삭제됨");
+//		System.out.println("####################################");
 		
 		//3. 멀티 select
-		List<Integer> selectList = Arrays.asList(7369, 7499, 7521);
-		List<EmpDTO>  selectMultiList = session.selectList("com.config.EmpMapper.selectMulti", selectList);
-		System.out.println(selectMultiList);
+//		System.out.println("3. 멀티 select ####################################");
+//		List<Integer> selectList = Arrays.asList(7369, 7499, 7521);
+//		List<EmpDTO>  selectMultiList = session.selectList("com.config.EmpMapper.selectMulti", selectList);
+//		System.out.println(selectMultiList);
+		System.out.println("####################################");
 		
+		//4. 동적 조건 지정
+//		System.out.println("4. 동적 조건 지정1 ( 조건 하나 )####################################");
+//		EmpDTO dto = new EmpDTO();
+//		//dto.setJob("SALESMAN");
+//		List<EmpDTO> selectMultiList2 = session.selectList("com.config.EmpMapper.selectIf", dto);
+//		System.out.println(selectMultiList2);
+//		System.out.println(selectMultiList2.size());
+		
+//		System.out.println("4. 동적 조건 지정2 ( 여러개 )####################################");
+//		EmpDTO dto2 = new EmpDTO();
+////		dto2.setJob("SALESMAN");
+////		dto2.setSal(1000);
+//		
+//		List<EmpDTO> selectMultiList3 = session.selectList("com.config.EmpMapper.selectIf2", dto2);
+//		System.out.println(selectMultiList3);
+//		System.out.println(selectMultiList3.size());
+		
+		
+		// 5. 동적수정
+		EmpDTO dto3 = new EmpDTO();
+//dto3.setSal(5000);
+dto3.setMgr(7369);
+		dto3.setEmpno(1003);
+		int n3 = session.update("com.config.EmpMapper.updateDynamic", dto3);
+		session.commit();
+		System.out.println(n3 +"가 수정됨");
 		
 		System.out.println("####################################");
-		List<EmpDTO> list = session.selectList("com.config.EmpMapper.findAll");
-		for (EmpDTO empDTO : list) {
-			System.out.println(empDTO);
-		}
+//		List<EmpDTO> list = session.selectList("com.config.EmpMapper.findAll");
+//		for (EmpDTO empDTO : list) {
+//			System.out.println(empDTO);
+//		}
 		
 	
 		
